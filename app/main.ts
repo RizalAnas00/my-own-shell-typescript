ff133e684261ee78f000b049057fefba0ef9041b
RizalAnas00 <belangsumerlang@gmail.com>
2025-12-26 22:55:04 +0700

    sixth stage

--- app/main.ts
+++ app/main.ts
@@ -5,10 +5,28 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+const validTypeCommands = ["echo", "type", "exit"];
+
+function handleTypeCommand(command: string) {
+  if (!validTypeCommands.includes(command)) {
+    rl.write(`${command}: not found\n`);
+    return;
+  } else {
+    rl.write(`${command}: is a shell builtin\n`);
+  }
+}
+
 function handleCommand(command: string, args: string[]) {
   switch (command) {
     case "echo":
         rl.write(args.join(" ") + "\n");
+        break;
+    case "type":
+        if (args.length === 0) {
+          rl.write("type: missing operand\n");
+        } else {
+          handleTypeCommand(args[0]);
+        }
         break;
     default:
         rl.write(`${command}: command not found\n`);
